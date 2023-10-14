b1dceced31d5295495433ac4e862aab93ad355c0
Han Chee Tan <hanchee@codecultivation.com>
2023-10-14 10:14:35 +0100

    Moved services and pipeline configuration inta an extension class

---
+++ Portal/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Portal.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        return app;
+    }
+}
--- Portal/Program.cs
+++ Portal/Program.cs
@@ -1,34 +1,9 @@
-namespace Portal
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+using Portal.Extensions;
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
 
-            var app = builder.Build();
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
-        }
-    }
-}
+app.Run();
